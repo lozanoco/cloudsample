2f0f943a9ca2c2bc46fe928123a86152e9eaf52d
apinazo <angelpinazo@gmail.com>
2018-12-10 16:44:27 +0100

    Add a test for the greetings service app.

--- greetings/src/test/java/es/apinazo/cloudsample/greetings/GreetingsAppTest.java
+++ greetings/src/test/java/es/apinazo/cloudsample/greetings/GreetingsAppTest.java
@@ -1,4 +1,4 @@
-package es.apinazo.cloudsample.discovery;
+package es.apinazo.cloudsample.greetings;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerAppTest {
+public class GreetingsAppTest {
 
     @Test
     public void contextLoads() {
