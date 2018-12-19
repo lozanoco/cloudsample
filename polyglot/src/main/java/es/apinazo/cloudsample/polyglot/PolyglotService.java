ba0e60f06eec3fc27827e12b38997b076f44b0c8
apinazo <angelpinazo@gmail.com>
2018-12-19 12:24:46 +0100

    Add comments.

--- polyglot/src/main/java/es/apinazo/cloudsample/polyglot/PolyglotService.java
+++ polyglot/src/main/java/es/apinazo/cloudsample/polyglot/PolyglotService.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
+/**
+ * Example application of Spring Cloud Netflix Sidecar.
+ *
+ * This app listens to a Node.js application located in the 'nodeapp' directory
+ * and registers it in the discovery server and so it will available through Zuul
+ * and could retrieve properties from the config server.
+ *
+ * Thus all the endpoints from the Node.js app will be available through the
+ * Zuul gateway.
+ */
 @EnableSidecar
 @SpringBootApplication
 public class PolyglotService {
